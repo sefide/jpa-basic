f7293760354e75fb78c187ab2f7dc8c2f97d6a48
sefide <sefide@naver.com>
2021-07-14 17:45:59 +0900

    feat: 준영속 상태 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -17,18 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setName("NANA");
-//            member.setId(7L);
-//
-//            System.out.println("BEFORE");
-//            em.persist(member);
-            Member find = em.find(Member.class, 7L);
-            Member find2 = em.find(Member.class, 7L);
-            System.out.println("find.id : " + find.getId());
-            System.out.println("find.name " + find.getName());
-            System.out.println("AFTER");
+            Member member = em.find(Member.class, 7L);
+            member.setName("NA");
 
+//            em.detach(member); // update 쿼리 발생 X
+            em.clear();
+
+            System.out.println("======================");
             tx.commit();
         } catch (Exception e) {
             System.out.println("ERROR");
