41afcd1f4f935df8cc927d28a1b57b7d408d045e
sefide <sefide@naver.com>
2021-08-09 22:58:55 +0900

    feat: 일대다 양방향

--- src/main/java/hellojpa/Membership.java
+++ src/main/java/hellojpa/Membership.java
@@ -12,6 +12,10 @@ public class Membership {
     @Column(name = "USERNAME")
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name="TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
     public Long getId() {
         return id;
     }
